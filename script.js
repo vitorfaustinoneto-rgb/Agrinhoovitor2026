da59f494654063a1f450a73ea8f04151b0229eca
vitorfaustinoneto-rgb <vitor.faustino.neto@escola.pr.gov.br>
2026-05-29 10:28:39 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+const botao = document.getElementById("botaoMensagem");
+
+botao.addEventListener("click", () => {
+  alert(
+    "🌱 O Agrinho 2026 incentiva a união entre tecnologia, produção agrícola e preservação ambiental!"
+  );
+});
